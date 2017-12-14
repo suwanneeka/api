df7df3c911f4869a33a56ce430b3f4a82b8da46a
nok <mynok2556@gmail.com>
2017-12-14 10:33:38 +0700

    commit1

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get ('/hi',function(){
+    echo 'ว่าไง';
+});
+
+$app->get ('/user/login',function(){
+    echo 'LOGIN';
+});
+
+$app->get ('/user/register',function(){
+    echo 'REGISTER';
+});
+
+$app->get ('/user/edit',function(){
+    echo 'EDIT';
+});
+
+$app->get ('/user/detail',function(){
+    echo 'DETAIL';
+});
+
+$app->get ('/user/history',function(){
+    echo 'HISTORY';
+});
+
+$app->get ('/user/editpass',function(){
+    echo 'EDITPASS';
+});
+
+$app->get ('/user/forgetpass',function(){
+    echo 'FORGETPASS';
+});
+
+$app->get ('/dog/new-dog',function(){
+    echo 'NEWDOG';
+});
+
+$app->get ('/dog/edit-dog',function(){
+    echo 'EDITDOG';
+});
+
+$app->get ('/dog/delete-dog',function(){
+    echo 'DELETEDOG';
+});
+
+$app->get ('/dog/search-dog',function(){
+    echo 'SEARCHDOG';
+});
+
+$app->get ('/dog/view-dog',function(){
+    echo 'VIEWDOG';
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
