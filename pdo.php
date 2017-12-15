dc9a2a5c466a3ca960126624d6e1f9e6d7b429fc
nok <mynok2556@gmail.com>
2017-12-15 15:19:12 +0700

    สำหรับเชื่อมต่อฐานข้อมูล

---
+++ pdo.php
@@ -0,0 +1,12 @@
+<?php
+
+//สร้าง PDO object เพื่อทำงานกับฐานข้อมูล
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'] .';charset=utf8',
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+?>
